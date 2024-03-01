127c058efe180c1997b59479e52f1b4556066240
Nick Eggleston <nickeggleston@yahoo.com>
2024-02-29 19:06:12 -0500

    product-routes DONE

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,12 +5,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
+  Product.findAll({
+    include: [
+     Category,
+     { model: Tag, through: ProductTag }, 
+    ],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      Category,
+      { model: Tag, through: ProductTag },
+    ]
+    })
+    .then((products) => res.json(products))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
@@ -87,12 +113,27 @@ router.put('/:id', (req, res) => {
       return res.json(product);
     })
     .catch((err) => {
+      console.log(err);
       // console.log(err);
       res.status(400).json(err);
     });
 });
 
 router.delete('/:id', (req, res) => {
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((products) => {
+      console.log(products);
+      res.json(products);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    })
+
   // delete one product by its `id` value
 });
 
